9a9d099d965877b369c200455608ab607d0b249d
Akerlem <111512662+Akerlem@users.noreply.github.com>
2024-01-24 11:27:27 +0200

    Create Independent amount of words for Text Animator.jsx
    Initial commit

---
+++ Independent amopunt of words for Text Animator.jsx
@@ -0,0 +1,12 @@
+//End:
+var textLayer = thisLayer.text.sourceText;
+var words = textLayer.value.split(/\s+/);
+100/words.length;
+
+//Offset:
+t1 = key(1).time;
+t2 = key(2).time;
+var textLayer = thisLayer.text.sourceText;
+var words = textLayer.value.split(/\s+/);
+l=(100/words.length)*(-1);
+linear(time,t1,t2,l,100)
